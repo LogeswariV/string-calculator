10448d0e4ee82bb140b2ec1ded4e05b9024f81b6
LogeswariV <logeswari03v@gmail.com>
2022-05-03 18:58:16 +0530

    initial commit

---
+++ src/component/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const StringCalculator = () => {
+    return <React.Fragment>
+        <h1>String Calculator</h1>
+    </React.Fragment>
+}
+export default StringCalculator;
